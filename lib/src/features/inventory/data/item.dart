2ed53c27c466ef4e3a987717df7c056c551b69c1
C2061455 <okeefesj@cardiff.ac.uk>
2023-02-23 22:40:59 +0000

    Create item class

---
+++ lib/src/features/inventory/data/item.dart
@@ -0,0 +1,9 @@
+class Item {
+  final String SKU;
+  final String businessId;
+  final String category;
+  final String name;
+  final int stock;
+
+  Item(this.SKU, this.businessId, this.category, this.name, this.stock);
+}
