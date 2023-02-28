240921953fa9e88740d027d23a4eb8226ba7f995
C2061455 <okeefesj@cardiff.ac.uk>
2023-02-28 16:51:56 +0000

    Add top bar

---
+++ lib/src/features/authentication/welcome_wave.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class WelcomeWave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String WaveSvgPath = 'lib/src/images/svgs/welcome_wave.svg';
+    final Widget WaveSvg = SvgPicture.asset(WaveSvgPath);
+    return (WaveSvg);
+  }
+}
--- lib/src/features/inventory/inventory_page.dart
+++ lib/src/features/inventory/inventory_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:instock_mobile/src/features/inventory/category_heading.dart';
 import 'package:instock_mobile/src/features/inventory/inventory_item.dart';
 import 'package:instock_mobile/src/features/inventory/services/inventory_service.dart';
 
 import '../../theme/common_theme.dart';
+import '../authentication/welcome_wave.dart';
 
 class Inventory extends StatefulWidget {
   const Inventory({super.key});
@@ -22,79 +24,125 @@ class _InventoryState extends State<Inventory> {
 
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-            child: Center(
-          child: Column(
-            children: [
-              const Text("Inventory"),
-              FutureBuilder(
-                  future: inventoryService.getItems(http.Client()),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.data == null) {
-                      return Center(
-                          child: CircularProgressIndicator(
-                        // backgroundColor: theme.themeData.splashColor,
-                        color: theme.themeData.splashColor,
-                      ));
-                    }
-                    return Flexible(
-                      child: ListView.builder(
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            bool isSameCategory = true;
-                            String category = snapshot.data[index].category;
-                            if (index == 0) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                                child: Column(
-                                  children: [
-                                    CategoryHeading(category: category),
-                                    InventoryItem(
-                                        itemName: snapshot.data[index].name,
-                                        itemSKU: snapshot.data[index].SKU,
-                                        itemStockNo: snapshot.data[index].stock,
-                                        itemOrdersNo: "N/A"),
-                                  ],
-                                ),
-                              );
-                            } else {
-                              String prevCategory =
-                                  snapshot.data[index - 1].category;
-                              isSameCategory = prevCategory == category;
-                            }
-                            if (isSameCategory == true) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                                child: InventoryItem(
-                                    itemName: snapshot.data[index].name,
-                                    itemSKU: snapshot.data[index].SKU,
-                                    itemStockNo: snapshot.data[index].stock,
-                                    itemOrdersNo: "N/A"),
-                              );
-                            } else if (isSameCategory == false) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                                child: Column(
-                                  children: [
-                                    CategoryHeading(category: category),
-                                    InventoryItem(
-                                        itemName: snapshot.data[index].name,
-                                        itemSKU: snapshot.data[index].SKU,
-                                        itemStockNo: snapshot.data[index].stock,
-                                        itemOrdersNo: "N/A"),
-                                  ],
-                                ),
-                              );
-                            }
-                          }),
-                    );
-                  }),
-            ],
-          ),
-        )),
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light
+              .copyWith(statusBarColor: theme.themeData.splashColor),
+          child: SafeArea(
+              child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  width: double.infinity,
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: Container(
+                          color: theme.themeData.splashColor,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                            child: Column(children: <Widget>[
+                              Text(
+                                "Inventory",
+                                style: theme.themeData.textTheme.headlineMedium
+                                    ?.merge(const TextStyle(fontSize: 24)),
+                                textAlign: TextAlign.center,
+                              ),
+                            ]),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        left: 10,
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: theme.themeData.primaryColorDark,
+                        ),
+                      ),
+                      Positioned(
+                        top: MediaQuery.of(context).size.height * 0.05 - 2,
+                        child: WelcomeWave(),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.06,
+                ),
+                FutureBuilder(
+                    future: inventoryService.getItems(http.Client()),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.data == null) {
+                        return Center(
+                            child: CircularProgressIndicator(
+                          // backgroundColor: theme.themeData.splashColor,
+                          color: theme.themeData.splashColor,
+                        ));
+                      }
+                      return Flexible(
+                        child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              bool isSameCategory = true;
+                              String category = snapshot.data[index].category;
+                              if (index == 0) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                                  child: Column(
+                                    children: [
+                                      CategoryHeading(category: category),
+                                      InventoryItem(
+                                          itemName: snapshot.data[index].name,
+                                          itemSKU: snapshot.data[index].SKU,
+                                          itemStockNo:
+                                              snapshot.data[index].stock,
+                                          itemOrdersNo: "N/A"),
+                                    ],
+                                  ),
+                                );
+                              } else {
+                                String prevCategory =
+                                    snapshot.data[index - 1].category;
+                                isSameCategory = prevCategory == category;
+                              }
+                              if (isSameCategory == true) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                                  child: InventoryItem(
+                                      itemName: snapshot.data[index].name,
+                                      itemSKU: snapshot.data[index].SKU,
+                                      itemStockNo: snapshot.data[index].stock,
+                                      itemOrdersNo: "N/A"),
+                                );
+                              } else if (isSameCategory == false) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                                  child: Column(
+                                    children: [
+                                      CategoryHeading(category: category),
+                                      InventoryItem(
+                                          itemName: snapshot.data[index].name,
+                                          itemSKU: snapshot.data[index].SKU,
+                                          itemStockNo:
+                                              snapshot.data[index].stock,
+                                          itemOrdersNo: "N/A"),
+                                    ],
+                                  ),
+                                );
+                              }
+                            }),
+                      );
+                    }),
+              ],
+            ),
+          )),
+        ),
       ),
     );
   }
