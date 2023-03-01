0cbc5e6d05502fcccd48fdeb6bcf6ae589851a3e
c2035950 <miaham@cardiff.ac.uk>
2023-03-01 20:22:42 +0000

    Style page with color scheme. Add common_theme.dart file

--- lib/src/features/business/add_business.dart
+++ lib/src/features/business/add_business.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
+import '../../theme/common_theme.dart';
+
 class AddBusiness extends StatefulWidget {
   const AddBusiness({super.key});
 
@@ -12,8 +14,9 @@ class AddBusiness extends StatefulWidget {
 }
 
 class _AddBusinessState extends State<AddBusiness> {
+  final theme = CommonTheme();
   final _formKey = GlobalKey<FormState>();
-  final avatarSize = 140.0;
+  final avatarSize = 150.0;
   File? image;
 
   // Taken from https://medium.com/unitechie/flutter-tutorial-image-picker-from-camera-gallery-c27af5490b74
@@ -38,6 +41,14 @@ class _AddBusinessState extends State<AddBusiness> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              const Text(
+                "Add Business",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+
+              const SizedBox(height: 40.0,),
+
               Container(
                 alignment: Alignment.center,
                 child: Stack(
@@ -49,10 +60,11 @@ class _AddBusinessState extends State<AddBusiness> {
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
                           color: Colors.grey.shade300,
+                          border: Border.all(color: theme.themeData.primaryColorDark)
                         ),
                         child: Center(
                           child: image == null
-                            ? const Text("No image selected.")
+                            ? const Icon(Icons.image_not_supported_outlined, size: 80.0,)
                             : CircleAvatar(
                               radius: avatarSize/2,
                               backgroundImage: FileImage(image!),
@@ -62,7 +74,7 @@ class _AddBusinessState extends State<AddBusiness> {
                     ),
                     Positioned(
                       bottom: 0.0,
-                      left: 80.0,
+                      left: 90.0,
                       child: ElevatedButton(
                         onPressed: () {
                           showModalBottomSheet<void>(
@@ -74,21 +86,42 @@ class _AddBusinessState extends State<AddBusiness> {
                             ),
                             builder: (BuildContext context) {
                               return SizedBox(
-                                height: 200,
-                                child: Center(
+                                height: 210,
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 30.0),
                                   child: Column(
                                     mainAxisAlignment: MainAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.stretch,
                                     children: <Widget>[
+                                      Align(
+                                        alignment: Alignment.topRight,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            shape: const CircleBorder(),
+                                            padding: const EdgeInsets.all(5),
+                                            backgroundColor: theme.themeData.primaryColorLight,
+                                          ),
+                                          child: const Icon(Icons.close, color: Colors.black),
+                                        ),
+                                      ),
+
+                                      const Divider(height: 10.0, ),
+
                                       ElevatedButton.icon(
                                         onPressed: () {
                                           pickImage(ImageSource.gallery);
                                         },
                                         icon: const Icon(Icons.image_outlined),
                                         label: const Text("Choose from Gallery"),
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: theme.themeData.primaryColorDark,
+                                        ),
                                       ),
 
-                                      const Text("or"),
+                                      const Text("or", textAlign: TextAlign.center,),
 
                                       ElevatedButton.icon(
                                         onPressed: () {
@@ -96,6 +129,9 @@ class _AddBusinessState extends State<AddBusiness> {
                                         },
                                         icon: const Icon(Icons.camera_alt),
                                         label: const Text("Take a Photo"),
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: theme.themeData.primaryColorDark,
+                                        ),
                                       ),
                                     ],
                                   ),
@@ -107,12 +143,12 @@ class _AddBusinessState extends State<AddBusiness> {
                         style: ElevatedButton.styleFrom(
                           shape: const CircleBorder(),
                           padding: const EdgeInsets.all(5),
-                          backgroundColor: Colors.green,
+                          backgroundColor: theme.themeData.splashColor,
+                          side: BorderSide(color: theme.themeData.primaryColorLight, width: 1),
                         ),
                         child: const Icon(
                           Icons.camera_alt_outlined,
                           size: 20,
-                          color: Colors.white,
                         ),
                       ),
                     ),
@@ -141,11 +177,14 @@ class _AddBusinessState extends State<AddBusiness> {
                         labelText: 'Enter the description',
                       ),
                     ),
-                    const SizedBox(height: 250.0,),
+                    const SizedBox(height: 180.0,),
                     ElevatedButton.icon(
                       onPressed: () { },
                       icon: const Icon(Icons.arrow_forward),
                       label: const Text("Continue"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: theme.themeData.splashColor,
+                      ),
                     ),
                   ],
                 ),
