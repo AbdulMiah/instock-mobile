952565598fd80e9ff904a56bb4beb7699233b4c6
c2035950 <miaham@cardiff.ac.uk>
2023-03-03 18:41:02 +0000

    Modify instock_text_input.dart to allow to specify maxLines for input field. Change add_business_page.dart to use InStockTextInput

--- lib/src/features/business/screens/add_business_page.dart
+++ lib/src/features/business/screens/add_business_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:instock_mobile/src/utilities/widgets/photo_picker.dart';
 
 import '../../../theme/common_theme.dart';
+import '../../../utilities/validation/validators.dart';
+import '../../../utilities/widgets/instock_text_input.dart';
 import '../../../utilities/widgets/wave.dart';
 
 class AddBusiness extends StatefulWidget {
@@ -13,8 +15,10 @@ class AddBusiness extends StatefulWidget {
 }
 
 class _AddBusinessState extends State<AddBusiness> {
-  final theme = CommonTheme();
   final _formKey = GlobalKey<FormState>();
+  final theme = CommonTheme();
+  String? _businessName;
+  String? _description;
 
   @override
   Widget build(BuildContext context) {
@@ -24,106 +28,100 @@ class _AddBusinessState extends State<AddBusiness> {
           value: SystemUiOverlayStyle.light
               .copyWith(statusBarColor: theme.themeData.splashColor),
           child: SafeArea(
-            child: Center(
-              child: Column(children: <Widget>[
-                Expanded(
-                  child: SizedBox(
+            child: SizedBox(
+              width: double.infinity,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  SizedBox(
                     width: double.infinity,
-                    child: Stack(
-                      clipBehavior: Clip.none,
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    child: Container(
+                      color: theme.themeData.splashColor,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                        child: Text(
+                          "Add Business",
+                          style: theme
+                              .themeData.textTheme.headlineMedium
+                              ?.merge(const TextStyle(fontSize: 24)),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.05 - 2,
+                    child: const InStockWave(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(
+                        60.0, 100.0, 60.0, 60.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        SizedBox(
-                          width: double.infinity,
-                          height: MediaQuery.of(context).size.height * 0.05,
-                          child: Container(
-                            color: theme.themeData.splashColor,
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                              child: Text(
-                                "Add Business",
-                                style: theme
-                                    .themeData.textTheme.headlineMedium
-                                    ?.merge(const TextStyle(fontSize: 24)),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ),
+                        const PhotoPicker(),
+                        const Divider(
+                          height: 50.0,
+                          thickness: 1.0,
                         ),
-                        Positioned(
-                          top: MediaQuery.of(context).size.height * 0.05 - 2,
-                          child: const InStockWave(),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(
-                              60.0, 100.0, 60.0, 60.0),
+                        Form(
+                          key: _formKey,
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            crossAxisAlignment:
+                                CrossAxisAlignment.stretch,
                             children: [
-                              const PhotoPicker(),
-                              const Divider(
-                                height: 50.0,
-                                thickness: 1.0,
-                              ),
-                              Form(
-                                key: _formKey,
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.stretch,
-                                  children: [
-                                    TextFormField(
-                                      decoration: const InputDecoration(
-                                        border: UnderlineInputBorder(),
-                                        labelText: 'Enter business name',
-                                      ),
-                                      validator: (value) {
-                                        if (value == null || value.isEmpty) {
-                                          return 'Please enter your business name';
-                                        }
-                                        return null;
-                                      },
-                                    ),
-                                    TextFormField(
-                                      minLines: 1,
-                                      maxLines: 4,
-                                      decoration: const InputDecoration(
-                                        border: UnderlineInputBorder(),
-                                        labelText: 'Enter the description',
-                                      ),
-                                      validator: (value) {
-                                        if (value == null || value.isEmpty) {
-                                          return 'Please enter your business description';
-                                        }
-                                        return null;
-                                      },
-                                    ),
+                              InStockTextInput(
+                                  text: 'Name',
+                                  theme: theme.themeData,
+                                  icon: null,
+                                  validators: const [
+                                    Validators.notNull,
+                                    Validators.notBlank,
+                                    Validators.shortLength,
                                   ],
-                                ),
-                              ),
-                              const Spacer(),
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                          content: Text('Processing Data')),
-                                    );
+                                  onSaved: (value) {
+                                    _businessName = value;
                                   }
-                                },
-                                icon: const Icon(Icons.arrow_forward),
-                                label: const Text("Continue"),
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      theme.themeData.primaryColorDark,
-                                ),
+                              ),
+                              InStockTextInput(
+                                  text: 'Description',
+                                  theme: theme.themeData,
+                                  icon: null,
+                                  maxLines: 4,
+                                  validators: const [
+                                    Validators.notNull,
+                                    Validators.notBlank,
+                                  ],
+                                  onSaved: (value) {
+                                    _description = value;
+                                  }
                               ),
                             ],
+                          ),
+                        ),
+                        const Spacer(),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Processing Data')),
+                              );
+                            }
+                          },
+                          icon: const Icon(Icons.arrow_forward),
+                          label: const Text("Continue"),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                theme.themeData.primaryColorDark,
                           ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ]),
+                ],
+              ),
             ),
           ),
         ),
--- lib/src/utilities/widgets/instock_text_input.dart
+++ lib/src/utilities/widgets/instock_text_input.dart
@@ -8,7 +8,8 @@ class InStockTextInput extends StatefulWidget {
       required this.icon,
       required this.validators,
       required this.onSaved,
-      this.obscureText = false});
+      this.obscureText = false,
+      this.maxLines});
 
   final ThemeData theme;
   final IconData? icon;
@@ -16,6 +17,7 @@ class InStockTextInput extends StatefulWidget {
   final List<Function> validators;
   final void Function(String?)? onSaved;
   bool obscureText = false;
+  final int? maxLines;
 
   @override
   State<InStockTextInput> createState() => _InStockTextInputState();
@@ -91,6 +93,8 @@ class _InStockTextInputState extends State<InStockTextInput> {
               displayIcon(),
               Expanded(
                 child: TextFormField(
+                  minLines: 1,
+                  maxLines: widget.maxLines ?? 1,
                   enableSuggestions: true,
                   autocorrect: true,
                   obscureText: widget.obscureText,
