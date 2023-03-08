95960687e256178c70d6d5f7252d185c1b7c3ce8
c2035950 <miaham@cardiff.ac.uk>
2023-03-08 11:55:32 +0000

    Connect API to front-end. Display appropriate messages to the client when retrieving responses from the API

--- lib/src/features/business/screens/add_business_page.dart
+++ lib/src/features/business/screens/add_business_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:instock_mobile/src/features/business/services/business_service.dart';
 import 'package:instock_mobile/src/utilities/widgets/photo_picker.dart';
 
 import '../../../theme/common_theme.dart';
+import '../../../utilities/objects/response_object.dart';
 import '../../../utilities/validation/validators.dart';
 import '../../../utilities/widgets/instock_button.dart';
 import '../../../utilities/widgets/instock_text_input.dart';
@@ -17,9 +19,47 @@ class AddBusiness extends StatefulWidget {
 
 class _AddBusinessState extends State<AddBusiness> {
   final _formKey = GlobalKey<FormState>();
-  final theme = CommonTheme();
+  final theme = CommonTheme().themeData;
   String? _businessName;
   String? _description;
+  String? _addBusinessError;
+  String? _addBusinessSuccess;
+
+  handleAddBusiness() async {
+    _addBusinessError = null;
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      BusinessService businessService = BusinessService();
+      ResponseObject response =
+      await businessService.addBusiness(_businessName!, _description!);
+      if (response.statusCode == 201) {
+        setState(() {
+          _addBusinessSuccess = "Successfully added a business to your account.";
+        });
+      } else if (response.statusCode == 401) {
+        setState(() {
+          _addBusinessError = "Whoops something went wrong, please try again";
+        });
+      } else if (response.statusCode == 400) {
+        setState(() {
+          _addBusinessError = "A Business is already associated with your account.";
+        });
+      } else {
+        setState(() {
+          _addBusinessError = response.message;
+        });
+      }
+    }
+  }
+
+  displayMessage(ThemeData theme) {
+    if (_addBusinessError != null) {
+      return Text(_addBusinessError!, style: theme.textTheme.headlineSmall);
+    } else if (_addBusinessSuccess != null) {
+      return Text(_addBusinessSuccess!, style: theme.textTheme.labelSmall);
+    }
+    return const Text("");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +67,7 @@ class _AddBusinessState extends State<AddBusiness> {
       home: Scaffold(
         body: AnnotatedRegion<SystemUiOverlayStyle>(
           value: SystemUiOverlayStyle.light
-              .copyWith(statusBarColor: theme.themeData.splashColor),
+              .copyWith(statusBarColor: theme.splashColor),
           child: SafeArea(
             child: SizedBox(
               width: double.infinity,
@@ -38,13 +78,12 @@ class _AddBusinessState extends State<AddBusiness> {
                     width: double.infinity,
                     height: MediaQuery.of(context).size.height * 0.05,
                     child: Container(
-                      color: theme.themeData.splashColor,
+                      color: theme.splashColor,
                       child: Padding(
                         padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
                         child: Text(
                           "Add Business",
-                          style: theme
-                              .themeData.textTheme.headlineMedium
+                          style: theme.textTheme.headlineMedium
                               ?.merge(const TextStyle(fontSize: 24)),
                           textAlign: TextAlign.center,
                         ),
@@ -74,7 +113,7 @@ class _AddBusinessState extends State<AddBusiness> {
                             children: [
                               InStockTextInput(
                                   text: 'Name',
-                                  theme: theme.themeData,
+                                  theme: theme,
                                   icon: null,
                                   validators: const [
                                     Validators.notNull,
@@ -87,7 +126,7 @@ class _AddBusinessState extends State<AddBusiness> {
                               ),
                               InStockTextInput(
                                   text: 'Description',
-                                  theme: theme.themeData,
+                                  theme: theme,
                                   icon: null,
                                   maxLines: 4,
                                   validators: const [
@@ -101,19 +140,15 @@ class _AddBusinessState extends State<AddBusiness> {
                             ],
                           ),
                         ),
+                        displayMessage(theme),
                         const Spacer(),
                         InStockButton(
                           text: 'Continue',
-                          theme: theme.themeData,
+                          theme: theme,
                           colorOption: InStockButton.primary,
                           icon: Icons.arrow_forward,
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text('Processing Data')),
-                              );
-                            }
+                          onPressed: () async {
+                              handleAddBusiness();
                           },
                         ),
                       ],
--- lib/src/features/business/services/business_service.dart
+++ lib/src/features/business/services/business_service.dart
@@ -1,20 +1,32 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 import '../../../utilities/objects/response_object.dart';
+import '../../authentication/services/authentication_service.dart';
 
 class BusinessService {
   Future<ResponseObject> addBusiness(String name, String description) async {
+    print("in business service");
+    var tokenDict = await AuthenticationService.retrieveBearerToken();
+    var token = tokenDict["bearerToken"];
+
     final url = Uri.parse('http://api.instockinventory.co.uk/business');
     var data = Map<String, dynamic>();
-    data['Name'] = name;
-    data['Description'] = description;
+    data['businessName'] = name;
+    data['businessDescription'] = description;
 
     var body = json.encode(data);
 
-    final response = await http.post(url,
-        headers: {"Content-Type": "application/json"}, body: body);
+    final response = await http.post(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+          "Content-Type": "application/json"
+        },
+        body: body
+    );
 
     ResponseObject responseObject =
     ResponseObject(response.statusCode, response.body);
