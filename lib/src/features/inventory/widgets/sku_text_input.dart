916b68ac49954dd071a48265c0e4ebf875f8f432
c2035950 <miaham@cardiff.ac.uk>
2023-03-14 12:57:12 +0000

    Update sku text input widget

--- lib/src/features/inventory/widgets/sku_text_input.dart
+++ lib/src/features/inventory/widgets/sku_text_input.dart
@@ -6,18 +6,23 @@ class SkuTextInput extends StatefulWidget {
   {super.key,
   required this.text,
   required this.theme,
-  required this.icon,
   required this.validators,
-  required this.onSaved,
+  this.textInputAction = TextInputAction.none,
+  this.onSaved,
+  this.onChanged,
+  this.icon,
+  this.boldLabel = false,
   this.initialValue,
-  this.enable = true,
   this.controller});
 
   final ThemeData theme;
-  final IconData? icon;
   final String text;
   final List<Function> validators;
   final void Function(String?)? onSaved;
+  final void Function(String?)? onChanged;
+  bool boldLabel = false;
+  IconData? icon;
+  TextInputAction textInputAction = TextInputAction.none;
   final String? initialValue;
   bool enable = true;
   final TextEditingController? controller;
@@ -27,8 +32,6 @@ class SkuTextInput extends StatefulWidget {
 }
 
 class _SkuTextInputState extends State<SkuTextInput> {
-  String? _errorMessage;
-
   displayIcon() {
     if (widget.icon != null) {
       return Icon(widget.icon);
@@ -37,44 +40,16 @@ class _SkuTextInputState extends State<SkuTextInput> {
   }
 
   String? runValidators(String? value) {
-    // Resets error message
-    setState(() {
-      _errorMessage = null;
-    });
-
     for (var i = 0; i < widget.validators.length;) {
       Function validator = widget.validators[i];
       String? res = validator(value);
       if (res != null) {
-        setState(() {
-          _errorMessage = res;
-        });
-
-        //We return an empty string to the validator
-        //so no error message is displayed from the text format
-        //but we set our custom error display to equal the error message
-        return "";
+        return res;
+      } else {
+        i++;
       }
-      i++;
     }
     return null;
-  }
-
-  displayErrorMessage() {
-    if (_errorMessage != null) {
-      return Padding(
-        padding: const EdgeInsets.fromLTRB(0, 4.0, 0, 0),
-        child: SizedBox(
-            width: 250,
-            child: Text('$_errorMessage',
-                style: widget.theme.textTheme.headlineSmall)),
-      );
-    }
-    // For whatever reason putting an empty container here instead
-    // causes the text to jump when switching to display text
-    // so this is a bit of a work around
-    // I don't think it's worth the extra time looking into why already spent an hour
-    return const Text("");
   }
 
   @override
@@ -82,51 +57,56 @@ class _SkuTextInputState extends State<SkuTextInput> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(widget.text, style: widget.theme.textTheme.bodySmall),
-        Container(
-          decoration: BoxDecoration(
-            border: Border(
-              bottom:
-              BorderSide(width: 1.0, color: widget.theme.primaryColorDark),
-            ),
-          ),
-          width: 250,
-          child: Row(
-            children: [
-              displayIcon(),
-              Expanded(
-                child: TextFormField(
-                    enabled: widget.enable,
-                    controller: widget.controller,
-                    initialValue: widget.initialValue,
-                    enableSuggestions: true,
-                    autocorrect: true,
-                    validator: (value) {
-                      return runValidators(value);
-                    },
-                    onSaved: widget.onSaved,
-                    cursorColor: widget.theme.primaryColorDark,
-                    decoration: InputDecoration(
-                      // If widget.icon is not given does not apply margin
-                      contentPadding: widget.icon != null
-                          ? const EdgeInsets.fromLTRB(4, 0, 0, 0)
-                          : null,
-                      border: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      errorStyle: const TextStyle(height: 0),
-                    ),
-                    inputFormatters: [
-                      LengthLimitingTextInputFormatter(15),
-                      UpperCaseTextFormatter()
-                    ],
-                ),
+        Text(widget.text,
+            style: widget.boldLabel
+                ? widget.theme.textTheme.labelMedium
+                : widget.theme.textTheme.bodySmall),
+        Row(
+          children: [
+            // displayIcon(),
+            Expanded(
+              child: TextFormField(
+                  style: widget.theme.textTheme.bodySmall,
+                  enabled: widget.enable,
+                  initialValue: widget.initialValue,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  onChanged: widget.onChanged,
+                  enableSuggestions: true,
+                  autocorrect: true,
+                  validator: (value) {
+                    return runValidators(value);
+                  },
+                  onSaved: widget.onSaved,
+                  cursorColor: widget.theme.primaryColorDark,
+                  textInputAction: widget.textInputAction,
+                  decoration: InputDecoration(
+                    prefixIcon: widget.icon != null ? Icon(widget.icon) : null,
+                    prefixIconConstraints:
+                    BoxConstraints.loose(Size.fromWidth(1000)),
+                    prefixIconColor: widget.theme.primaryColorDark,
+                    errorStyle: widget.theme.textTheme.headlineSmall,
+                    errorMaxLines: 5,
+                    border: UnderlineInputBorder(
+                        borderSide:
+                        BorderSide(color: widget.theme.primaryColorDark)),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                        BorderSide(color: widget.theme.primaryColorDark)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                        BorderSide(color: widget.theme.primaryColorDark)),
+                    errorBorder: UnderlineInputBorder(
+                        borderSide:
+                        BorderSide(color: widget.theme.highlightColor)),
+                  ),
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(6),
+                    UpperCaseTextFormatter()
+                  ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        displayErrorMessage(),
       ],
     );
   }
