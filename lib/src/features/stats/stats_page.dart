f872be04b8896e5564619dae46d025958ccf354f
C2061455 <okeefesj@cardiff.ac.uk>
2023-03-22 16:06:51 +0000

    Add stats heading

--- lib/src/features/stats/stats_page.dart
+++ lib/src/features/stats/stats_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../theme/common_theme.dart';
+import '../../utilities/widgets/wave.dart';
 
 class StatsPage extends StatefulWidget {
   const StatsPage({super.key});
@@ -10,9 +14,50 @@ class StatsPage extends StatefulWidget {
 class _StatsPageState extends State<StatsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Stats"),
+    final theme = CommonTheme();
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light
+            .copyWith(statusBarColor: theme.themeData.splashColor),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.05,
+                          child: Container(
+                            color: theme.themeData.splashColor,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                              child: Text(
+                                "Stats",
+                                style: theme.themeData.textTheme.bodyMedium
+                                    ?.merge(const TextStyle(fontSize: 24)),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          width: MediaQuery.of(context).size.width,
+                          top: MediaQuery.of(context).size.height * 0.05 - 2,
+                          child: const InStockWave(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
