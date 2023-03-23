69d947a16a05bfabcd9eb61ecff2acff67f91d47
C2061455 <okeefesj@cardiff.ac.uk>
2023-03-23 17:00:46 +0000

    Start displaying category stats

--- lib/src/features/stats/screens/stats_page.dart
+++ lib/src/features/stats/screens/stats_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:instock_mobile/src/features/stats/services/stats_service.dart';
+import 'package:instock_mobile/src/features/stats/widgets/category_stats.dart';
 import 'package:instock_mobile/src/features/stats/widgets/overview_stats.dart';
 
 import '../../../theme/common_theme.dart';
@@ -20,6 +21,9 @@ class StatsPage extends StatefulWidget {
 
 class _StatsPageState extends State<StatsPage> {
   final StatsService _statsService = StatsService(AuthenticationService());
+
+  List<String>? categories;
+  String? dropdownValue;
   @override
   Widget build(BuildContext context) {
     final theme = CommonTheme();
@@ -29,97 +33,133 @@ class _StatsPageState extends State<StatsPage> {
             .copyWith(statusBarColor: theme.themeData.splashColor),
         child: SafeArea(
           child: Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  width: double.infinity,
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    children: [
-                      SizedBox(
-                        width: double.infinity,
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: Container(
-                          color: theme.themeData.splashColor,
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                            child: Text(
-                              "Stats",
-                              style: theme.themeData.textTheme.bodyMedium
-                                  ?.merge(const TextStyle(fontSize: 24)),
-                              textAlign: TextAlign.center,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    width: double.infinity,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.05,
+                          child: Container(
+                            color: theme.themeData.splashColor,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                              child: Text(
+                                "Stats",
+                                style: theme.themeData.textTheme.bodyMedium
+                                    ?.merge(const TextStyle(fontSize: 24)),
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Positioned(
-                        width: MediaQuery.of(context).size.width,
-                        top: MediaQuery.of(context).size.height * 0.05 - 2,
-                        child: const InStockWave(),
-                      ),
-                    ],
+                        Positioned(
+                          width: MediaQuery.of(context).size.width,
+                          top: MediaQuery.of(context).size.height * 0.05 - 2,
+                          child: const InStockWave(),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 10),
-                  child: Text(
-                    "Shop Overview",
-                    style: theme.themeData.textTheme.headlineMedium
-                        ?.merge(const TextStyle(fontSize: 24)),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 10),
+                    child: Text(
+                      "Shop Overview",
+                      style: theme.themeData.textTheme.headlineMedium
+                          ?.merge(const TextStyle(fontSize: 24)),
+                    ),
                   ),
-                ),
-                FutureBuilder(
-                    future: _statsService.getStats(http.Client()),
-                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                      if (snapshot.data == null &&
-                          snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(
-                          child: CircularProgressIndicator(
-                            color: theme.themeData.splashColor,
-                          ),
+                  FutureBuilder(
+                      future: _statsService.getStats(http.Client()),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.data == null &&
+                            snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: theme.themeData.splashColor,
+                            ),
+                          );
+                        }
+                        if (snapshot.error is SocketException) {
+                          return Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "No Internet Connection",
+                                  style: theme.themeData.textTheme.bodyLarge
+                                      ?.merge(const TextStyle(fontSize: 30)),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  "Please check your internet connection and try again",
+                                  style: theme.themeData.textTheme.bodySmall,
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 40),
+                                InStockButton(
+                                  onPressed: () {
+                                    setState(() {});
+                                  },
+                                  theme: theme.themeData,
+                                  colorOption: InStockButton.primary,
+                                  text: "Try Again",
+                                  icon: Icons.refresh,
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                        categories = ["Cards", "Stickers", "Bookmarks"];
+                        dropdownValue = categories?.first;
+                        print("categoes: $categories");
+                        return Column(
+                          children: [
+                            OverviewStats(
+                                statsDto: snapshot.data, theme: theme),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                              child: Text(
+                                "Performance By Category",
+                                style: theme.themeData.textTheme.headlineMedium
+                                    ?.merge(const TextStyle(fontSize: 24)),
+                              ),
+                            ),
+                            DropdownButton<String>(
+                              value: dropdownValue,
+                              icon: const Icon(Icons.arrow_downward),
+                              elevation: 16,
+                              style: const TextStyle(color: Colors.deepPurple),
+                              underline: Container(
+                                height: 2,
+                                color: Colors.deepPurpleAccent,
+                              ),
+                              onChanged: (String? value) {
+                                // This is called when the user selects an item.
+                                setState(() {
+                                  dropdownValue = value!;
+                                });
+                              },
+                              items: categories!.map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                            ),
+                            CategoryStats(statsDto: snapshot.data, theme: theme)
+                          ],
                         );
-                      }
-                      if (snapshot.error is SocketException) {
-                        return Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "No Internet Connection",
-                                style: theme.themeData.textTheme.bodyLarge
-                                    ?.merge(const TextStyle(fontSize: 30)),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 20),
-                              Text(
-                                "Please check your internet connection and try again",
-                                style: theme.themeData.textTheme.bodySmall,
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 40),
-                              InStockButton(
-                                onPressed: () {
-                                  setState(() {});
-                                },
-                                theme: theme.themeData,
-                                colorOption: InStockButton.primary,
-                                text: "Try Again",
-                                icon: Icons.refresh,
-                              ),
-                            ],
-                          ),
-                        );
-                      }
-
-                      return OverviewStats(
-                          statsDto: snapshot.data, theme: theme);
-                    }),
-                // Text(
-                //   "Performance By Category",
-                //   style: theme.themeData.textTheme.headlineMedium
-                //       ?.merge(const TextStyle(fontSize: 24)),
-                // ),
-              ],
+                      }),
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/src/features/stats/widgets/category_stats.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:instock_mobile/src/features/stats/data/stats_dto.dart';
+import 'package:instock_mobile/src/features/stats/widgets/stats_box.dart';
+
+import '../../../theme/common_theme.dart';
+
+class CategoryStats extends StatelessWidget {
+  final StatsDto statsDto;
+  CategoryStats({
+    super.key,
+    required this.statsDto,
+    required this.theme,
+  });
+
+  final CommonTheme theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: theme.themeData.splashColor,
+              borderRadius: BorderRadius.circular(5)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+                "Sales: ${statsDto.performanceByCategory["cards"]["sales"]}",
+                style: theme.themeData.textTheme.displayMedium
+                    ?.merge(const TextStyle(fontSize: 24))),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            StatsBox(
+              theme: theme,
+              stat: "Orders",
+              figure: statsDto.overallShopPerformance["orders"]!,
+            ),
+            StatsBox(
+              theme: theme,
+              stat: "Corrections",
+              figure: 3,
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              StatsBox(
+                theme: theme,
+                stat: "Returns",
+                figure: statsDto.overallShopPerformance["returns"]!,
+              ),
+              StatsBox(
+                theme: theme,
+                stat: "Giveaways",
+                figure: 3,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              StatsBox(
+                theme: theme,
+                stat: "Damaged",
+                figure: statsDto.overallShopPerformance["damaged"]!,
+              ),
+              StatsBox(
+                theme: theme,
+                stat: "Restocked",
+                figure: statsDto.overallShopPerformance["restocked"]!,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              StatsBox(
+                theme: theme,
+                stat: "Lost",
+                figure: statsDto.overallShopPerformance["lost"]!,
+              ),
+              const SizedBox(
+                width: 150,
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
