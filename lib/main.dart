adba3c922e9ca2fc1aafd63f47c1e3e59cde6be9
Archie <archie.wilkins17@gmail.com>
2023-03-23 20:13:46 +0000

    started breaking notifications lol

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:instock_mobile/src/features/auth_check.dart';
-import 'package:instock_mobile/src/utilities/widgets/notif.dart';
 
 import 'injection.dart';
 
-// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-// @pragma('vm:entry-point')
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   // If you're going to use other Firebase services in the background, such as Firestore,
-//   // make sure you call `initializeApp` before using other Firebase services.
-//   await Firebase.initializeApp();
-//
-//   print("Handling a background message: ${message.messageId}");
-// }
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
-    Firebase.initializeApp();
     runApp(const MyApp());
   });
 }
@@ -38,7 +28,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
     // Firebase.initializeApp();
     // Notif.initialize(flutterLocalNotificationsPlugin);
