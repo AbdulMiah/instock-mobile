909d84d7134b1ff73d28a2128f66a4fadbf82c5c
C2061455 <okeefesj@cardiff.ac.uk>
2023-03-24 10:55:23 +0000

    Fix loading positioning

--- lib/src/features/stats/screens/stats_page.dart
+++ lib/src/features/stats/screens/stats_page.dart
@@ -53,186 +53,178 @@ class _StatsPageState extends State<StatsPage> {
         value: SystemUiOverlayStyle.light
             .copyWith(statusBarColor: theme.themeData.splashColor),
         child: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    width: double.infinity,
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          height: MediaQuery.of(context).size.height * 0.05,
-                          child: Container(
-                            color: theme.themeData.splashColor,
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                              child: Text(
-                                "Stats",
-                                style: theme.themeData.textTheme.bodyMedium
-                                    ?.merge(const TextStyle(fontSize: 24)),
-                                textAlign: TextAlign.center,
-                              ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  width: double.infinity,
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: Container(
+                          color: theme.themeData.splashColor,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                            child: Text(
+                              "Stats",
+                              style: theme.themeData.textTheme.bodyMedium
+                                  ?.merge(const TextStyle(fontSize: 24)),
+                              textAlign: TextAlign.center,
                             ),
                           ),
                         ),
-                        Positioned(
-                          width: MediaQuery.of(context).size.width,
-                          top: MediaQuery.of(context).size.height * 0.05 - 2,
-                          child: const InStockWave(),
-                        ),
-                      ],
-                    ),
+                      ),
+                      Positioned(
+                        width: MediaQuery.of(context).size.width,
+                        top: MediaQuery.of(context).size.height * 0.05 - 2,
+                        child: const InStockWave(),
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 10),
-                    child: Text(
-                      "Shop Overview",
-                      style: theme.themeData.textTheme.headlineMedium
-                          ?.merge(const TextStyle(fontSize: 24)),
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 10),
+                  child: Text(
+                    "Shop Overview",
+                    style: theme.themeData.textTheme.headlineMedium
+                        ?.merge(const TextStyle(fontSize: 24)),
                   ),
-                  FutureBuilder(
-                      future: _statsService.getStats(http.Client()),
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        if (snapshot.data == null &&
-                            snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                          return Center(
-                            child: CircularProgressIndicator(
-                              color: theme.themeData.splashColor,
-                            ),
-                          );
-                        }
-                        if (snapshot.error is SocketException) {
-                          return Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "No Internet Connection",
-                                  style: theme.themeData.textTheme.bodyLarge
-                                      ?.merge(const TextStyle(fontSize: 30)),
-                                  textAlign: TextAlign.center,
-                                ),
-                                const SizedBox(height: 20),
-                                Text(
-                                  "Please check your internet connection and try again",
-                                  style: theme.themeData.textTheme.bodySmall,
-                                  textAlign: TextAlign.center,
-                                ),
-                                const SizedBox(height: 40),
-                                InStockButton(
-                                  onPressed: () {
-                                    setState(() {});
-                                  },
-                                  theme: theme.themeData,
-                                  colorOption: InStockButton.primary,
-                                  text: "Try Again",
-                                  icon: Icons.refresh,
-                                ),
-                              ],
-                            ),
-                          );
-                        }
-                        StatsDto statsDto = snapshot.data;
-                        // Set dropdown list to categories from request
-                        List<String> dropdownItems =
-                            extractCategories(statsDto);
-                        // If the user hasn't made a selection, default to first item
-                        lastSelected ??= dropdownItems.first;
-                        _dropdownCategory = lastSelected!;
-                        return Column(
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.fromLTRB(0, 8.0, 0, 16.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                  color: theme.themeData.cardColor,
-                                ),
-                                child: Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          0, 12.0, 0, 0),
-                                      child: Text("Shop Performance",
-                                          style: theme.themeData.textTheme
-                                              .headlineMedium),
+                ),
+                FutureBuilder(
+                    future: _statsService.getStats(http.Client()),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.data == null &&
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            color: theme.themeData.splashColor,
+                          ),
+                        );
+                      }
+                      if (snapshot.error is SocketException) {
+                        return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "No Internet Connection",
+                                style: theme.themeData.textTheme.bodyLarge
+                                    ?.merge(const TextStyle(fontSize: 30)),
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 20),
+                              Text(
+                                "Please check your internet connection and try again",
+                                style: theme.themeData.textTheme.bodySmall,
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(height: 40),
+                              InStockButton(
+                                onPressed: () {
+                                  setState(() {});
+                                },
+                                theme: theme.themeData,
+                                colorOption: InStockButton.primary,
+                                text: "Try Again",
+                                icon: Icons.refresh,
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      StatsDto statsDto = snapshot.data;
+                      // Set dropdown list to categories from request
+                      List<String> dropdownItems = extractCategories(statsDto);
+                      // If the user hasn't made a selection, default to first item
+                      lastSelected ??= dropdownItems.first;
+                      _dropdownCategory = lastSelected!;
+                      return Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 16.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        0, 12.0, 0, 0),
+                                    child: Text("Shop Performance",
+                                        style: theme.themeData.textTheme
+                                            .headlineMedium),
+                                  ),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.85,
+                                    height: 300,
+                                    child: ShopPerformanceGraph(
+                                      salesByMonth: statsDto.salesByMonth,
+                                      deductionsByMonth:
+                                          statsDto.deductionsByMonth,
                                     ),
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.85,
-                                      height: 300,
-                                      child: ShopPerformanceGraph(
-                                        salesByMonth: statsDto.salesByMonth,
-                                        deductionsByMonth:
-                                            statsDto.deductionsByMonth,
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
-                            OverviewStats(
-                                statsDto: snapshot.data, theme: theme),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                              child: Text(
-                                "Performance By Category",
-                                style: theme.themeData.textTheme.headlineMedium
-                                    ?.merge(const TextStyle(fontSize: 24)),
-                              ),
+                          ),
+                          OverviewStats(statsDto: snapshot.data, theme: theme),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                            child: Text(
+                              "Performance By Category",
+                              style: theme.themeData.textTheme.headlineMedium
+                                  ?.merge(const TextStyle(fontSize: 24)),
                             ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.fromLTRB(12.0, 0, 0, 12),
-                              child: SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.6,
-                                height: 50,
-                                child: DropdownButton(
-                                  value: lastSelected,
-                                  icon: const Icon(Icons.keyboard_arrow_down),
-                                  items: dropdownItems.map((String items) {
-                                    return DropdownMenuItem(
-                                      value: items,
-                                      child: Text(items),
-                                    );
-                                  }).toList(),
-                                  onChanged: (String? newValue) {
-                                    setState(() {
-                                      _updateCategory(newValue!);
-                                      lastSelected = newValue;
-                                    });
-                                  },
-                                  style: theme.themeData.textTheme.bodySmall,
-                                  dropdownColor:
-                                      theme.themeData.primaryColorLight,
-                                  iconEnabledColor:
-                                      theme.themeData.primaryColorDark,
-                                  isExpanded: true,
-                                  underline: Container(
-                                    height: 2,
-                                    decoration: BoxDecoration(
-                                      color: theme.themeData.primaryColorDark,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(12.0, 0, 0, 12),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.6,
+                              height: 50,
+                              child: DropdownButton(
+                                value: lastSelected,
+                                icon: const Icon(Icons.keyboard_arrow_down),
+                                items: dropdownItems.map((String items) {
+                                  return DropdownMenuItem(
+                                    value: items,
+                                    child: Text(items),
+                                  );
+                                }).toList(),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    _updateCategory(newValue!);
+                                    lastSelected = newValue;
+                                  });
+                                },
+                                style: theme.themeData.textTheme.bodySmall,
+                                dropdownColor:
+                                    theme.themeData.primaryColorLight,
+                                iconEnabledColor:
+                                    theme.themeData.primaryColorDark,
+                                isExpanded: true,
+                                underline: Container(
+                                  height: 2,
+                                  decoration: BoxDecoration(
+                                    color: theme.themeData.primaryColorDark,
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
                                 ),
                               ),
                             ),
-                            CategoryStats(
-                                statsDto: snapshot.data,
-                                updateCategory: _updateCategory,
-                                dropdownCategory: _dropdownCategory!)
-                          ],
-                        );
-                      }),
-                ],
-              ),
+                          ),
+                          CategoryStats(
+                              statsDto: snapshot.data,
+                              updateCategory: _updateCategory,
+                              dropdownCategory: _dropdownCategory!)
+                        ],
+                      );
+                    }),
+              ],
             ),
           ),
         ),
