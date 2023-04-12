0971fb1ad7bd30458ca85e8ad921409426f608c0
c2035950 <miaham@cardiff.ac.uk>
2023-04-12 23:18:31 +0100

    Add user email at the end of email body

--- lib/src/features/account/services/email_service.dart
+++ lib/src/features/account/services/email_service.dart
@@ -1,17 +1,27 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:jwt_decode/jwt_decode.dart';
 import '../../../utilities/objects/response_object.dart';
 import '../../../utilities/services/config_service.dart';
+import '../../authentication/services/authentication_service.dart';
+import '../../authentication/services/interfaces/Iauthentication_service.dart';
 
 class EmailService {
+  final IAuthenticationService _authenticationService = AuthenticationService();
+
   Future<ResponseObject> sendMessage(String topic, String message) async {
+    var tokenDict = await _authenticationService.retrieveBearerToken();
+    var token = tokenDict["bearerToken"];
+    Map<String, dynamic> payload = Jwt.parseJwt(token);
+    String email = payload["Email"];
+
     String url = ConfigService.url;
     final uri = Uri.parse('$url/contact-us');
 
     var data = Map<String, dynamic>();
     data['Topic'] = topic;
-    data['Message'] = message;
+    data['Message'] = "$message\n\n- From $email";
 
     var body = json.encode(data);
 
