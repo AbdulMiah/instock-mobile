157daec22e39d042ab48afc69e89905f5ce8a246
C2061455 <okeefesj@cardiff.ac.uk>
2023-04-13 12:55:23 +0100

    Display suggestion and set colour based on type

--- lib/src/features/stats/widgets/suggestions_carousel.dart
+++ lib/src/features/stats/widgets/suggestions_carousel.dart
@@ -2,6 +2,8 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:instock_mobile/src/features/stats/data/stats_dto.dart';
 
+import '../../../theme/common_theme.dart';
+
 class SuggestionsCarousel extends StatelessWidget {
   final StatsDto statsDto;
 
@@ -9,19 +11,29 @@ class SuggestionsCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map<String, String> suggestionsDict = extractSuggestions(statsDto);
-    print(suggestionsDict);
+    final theme = CommonTheme();
+    List<Map<String, String>> suggestionsMaps = extractSuggestions(statsDto);
+
     return CarouselSlider(
       options: CarouselOptions(height: 400.0),
-      items: [1, 2, 3, 4, 5].map((i) {
+      items: suggestionsMaps.map((i) {
         return Builder(
           builder: (BuildContext context) {
+            String positiveOrNegative = i.keys.first;
+            String suggestionText = i.values.first;
             return Container(
                 width: MediaQuery.of(context).size.width,
+                height: null,
                 margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(color: Colors.amber),
+                decoration: BoxDecoration(
+                  color: positiveOrNegative == ('Positive')
+                      ? theme.themeData.splashColor
+                      : suggestionText == ('Negative')
+                          ? theme.themeData.cardColor
+                          : theme.themeData.cardColor,
+                ),
                 child: Text(
-                  'text $i',
+                  suggestionText,
                   style: TextStyle(fontSize: 16.0),
                 ));
           },
@@ -30,10 +42,9 @@ class SuggestionsCarousel extends StatelessWidget {
     );
   }
 
-  Map<String, String> extractSuggestions(StatsDto statsDto) {
-    Map<String, String> res = {};
+  List<Map<String, String>> extractSuggestions(StatsDto statsDto) {
+    List<Map<String, String>> res = [];
     for (var key in statsDto.suggestions.keys) {
-      // print(statsDto.suggestions[key]);
       // if there is a suggestion
       if (statsDto.suggestions[key].length > 0) {
         switch (key) {
@@ -42,14 +53,20 @@ class SuggestionsCarousel extends StatelessWidget {
             String itemName = statsDto.suggestions[key][sales]["name"];
             String suggestion =
                 "With $sales sales, $itemName is your best seller!";
-            res[suggestion] = "Positive";
+            Map<String, String> sugMap = {
+              'Positive': suggestion,
+            };
+            res.add(sugMap);
             break;
           case "worstSellingItem":
             String sales = statsDto.suggestions[key].keys.first;
             String itemName = statsDto.suggestions[key][sales]["name"];
             String suggestion =
                 "With $sales sales, $itemName is your worst seller";
-            res[suggestion] = "Negative";
+            Map<String, String> sugMap = {
+              'Negative': suggestion,
+            };
+            res.add(sugMap);
             break;
           case "itemToRestock":
             String ratio = statsDto.suggestions[key].keys.first;
@@ -57,35 +74,50 @@ class SuggestionsCarousel extends StatelessWidget {
             String suggestion =
                 "$itemName is flying off the shelves! We predict this will be "
                 "your next item to run out of stock. Better restock soon!";
-            res[suggestion] = "Positive";
+            Map<String, String> sugMap = {
+              'Positive': suggestion,
+            };
+            res.add(sugMap);
             break;
           case "longestNoSales":
             String days = statsDto.suggestions[key].keys.first;
             String itemName = statsDto.suggestions[key][days]["name"];
             String suggestion =
                 "$itemName hasn't sold in $days. People don't know what they're missing!";
-            res[suggestion] = "Negative";
+            Map<String, String> sugMap = {
+              'Negative': suggestion,
+            };
+            res.add(sugMap);
             break;
           case "bestSellingCategory":
             String sales = statsDto.suggestions[key].keys.first;
             String category = statsDto.suggestions[key][sales];
             String suggestion =
                 "With $sales sales, $category is your best selling category!";
-            res[suggestion] = "Positive";
+            Map<String, String> sugMap = {
+              'Positive': suggestion,
+            };
+            res.add(sugMap);
             break;
           case "worstSellingCategory":
             String sales = statsDto.suggestions[key].keys.first;
             String category = statsDto.suggestions[key][sales];
             String suggestion =
                 "With $sales sales, $category is not a popular category";
-            res[suggestion] = "Negative";
+            Map<String, String> sugMap = {
+              'Negative': suggestion,
+            };
+            res.add(sugMap);
             break;
           case "mostReturns":
             String returns = statsDto.suggestions[key].keys.first;
             String itemName = statsDto.suggestions[key][returns]["name"];
             String suggestion =
                 "$itemName has had $returns returns. This is your most returned item.";
-            res[suggestion] = "Negative";
+            Map<String, String> sugMap = {
+              'Negative': suggestion,
+            };
+            res.add(sugMap);
             break;
         }
       }
