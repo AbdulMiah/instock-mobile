781ae54e325c10494eeeadf48c26db3f9e518ac0
c2035950 <miaham@cardiff.ac.uk>
2023-04-29 15:02:28 +0100

    Add info icon to shop connections which shows an alert dialog when tapped.

--- lib/src/features/business/screens/shop_connections.dart
+++ lib/src/features/business/screens/shop_connections.dart
@@ -4,6 +4,7 @@ import 'package:instock_mobile/src/features/business/screens/business_page.dart'
 
 import '../../../theme/common_theme.dart';
 import '../../../utilities/widgets/back_button.dart';
+import '../../../utilities/widgets/instock_button.dart';
 import '../../../utilities/widgets/wave.dart';
 import '../widgets/shop_connection_list.dart';
 
@@ -54,7 +55,46 @@ class _ShopConnectionsState extends State<ShopConnections> {
                             child: InStockBackButton(
                               page: BusinessPage(),
                               colorOption: InStockBackButton.secondary,
-                            )),
+                            )
+                        ),
+                        Positioned(
+                            top: 10,
+                            right: 10,
+                            child: GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text(
+                                        "Connections Info",
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      actions: [
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 16.0),
+                                          child: Text(
+                                            "If you connect to a platform and create a new item with the same SKU as an item already on that platform, the two items will be linked together. Your items can be linked to multiple platforms simultaneously. Once they're linked, you'll be able to see the total number of pending orders from all platforms, and the platform's listed item quantity will automatically adjust to match the available stock (Total stock you listed - Live Orders = Available Stock). \n \nIf you change the total stock you have listed that will in turn change the available stock for your platforms and will cause that platforms stock listing amount to be updated.",
+                                            style: theme.themeData.textTheme.bodySmall?.copyWith(fontSize: 16),
+                                          ),
+                                        ),
+                                        Center(
+                                          child: InStockButton(
+                                            text: "Ok",
+                                            onPressed: () {
+                                              Navigator.pop(context); // Close the AlertDialog
+                                            },
+                                            theme: theme.themeData,
+                                            colorOption: InStockButton.accent,
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  });
+                              },
+                              child: const Icon(Icons.info_outline)
+                            ),
+                        ),
                         Positioned(
                           width: MediaQuery.of(context).size.width,
                           top: MediaQuery.of(context).size.height * 0.05 - 2,
